73c2d69da89304b2bc60761c67a9ab8fdb4f4b9f
Yurii <yvla@MacBook-Pro-Yurii.local>
2026-07-28 00:12:47 +0400

    feat: add query for dao

--- app/src/main/java/com/yvl/notes/data/NoteDbModel.kt
+++ app/src/main/java/com/yvl/notes/data/NoteDbModel.kt
@@ -3,7 +3,7 @@ package com.yvl.notes.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "notes")
 data class NoteDbModel(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
--- app/src/main/java/com/yvl/notes/data/NotesDao.kt
+++ app/src/main/java/com/yvl/notes/data/NotesDao.kt
@@ -1,4 +1,25 @@
 package com.yvl.notes.data
 
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface NotesDao {
+
+    @Query("SELECT * FROM notes ORDER BY updatedAt DESC")
+    fun getAllNotes(): Flow<List<NoteDbModel>>
+
+    @Query("SELECT * FROM notes WHERE title LIKE '%' || :query || '%' OR content LIKE '%' || :query || '%' ORDER BY updatedAt DESC")
+    fun searchNotes(query: String): Flow<List<NoteDbModel>>
+
+    @Query("DELETE FROM notes WHERE id == :noteId")
+    suspend fun deleteNote(noteId: Int)
+
+    @Query("UPDATE notes SET isPinned = NOT isPinned WHERE id == :noteId")
+    suspend fun switchPinnedStatus(noteId: Int)
+
+    @Upsert
+    suspend fun addNote(noteDbModel: NoteDbModel)
 }
